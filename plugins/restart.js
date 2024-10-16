e72ef5b45d4aefec6a3b2c2bf7e4cf4794eff2f0
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-16 20:46:05 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -4,14 +4,15 @@ const {sleep} = require('../lib/functions')
 
 cmd({
     pattern: "restart",
-    desc: "restart the bot",
+    desc: "Restart the DENETH-MD BOT",
+    react: "🔄",
     category: "owner",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const {exec} = require("child_process")
-reply("restarting...")
+reply("Restarting...")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
